3f69085ddf5256bb55cba4a9cc757f07cebdda60
王晋标 <2463724836@qq.com>
2017-07-24 17:11:35 +0800

    定义常量w = 0

--- WXMovie/MainTabbarController.m
+++ WXMovie/MainTabbarController.m
@@ -19,6 +19,9 @@
     [super viewDidLoad];
     
     self.view.backgroundColor = [UIColor blackColor];
+    
+    NSInteger w = 0;
+    NSLog(@"%zd",w);
 //    self.viewControllers;
     
     //     UI_APPEARANCE_SELECTOR   每一个导航栏都调用设置图片的方法
