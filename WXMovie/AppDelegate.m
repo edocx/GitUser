23a336749262b1b7acab12af35eb4b553f6f567c
王晋标 <2463724836@qq.com>
2017-07-24 12:48:21 +0800

    wangjinbiao  tiajiao zuixinf

--- WXMovie/AppDelegate.m
+++ WXMovie/AppDelegate.m
@@ -3,7 +3,7 @@
 //  WXMovie
 //
 //  Created by imac on 15/9/7.
-//  Copyright (c) 2015年 www.iphonetrain.com 无限互联3G学院. All rights reserved.
+//  Copyright (c) 2334 年 www.iphonetrain.com 无限互联3G学院. All rights reserved.
 //
 
 #import "AppDelegate.h"
