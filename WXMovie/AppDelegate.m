1ab8ff3ec3da556138092efbc5be2ce32b80effa
王晋标 <2463724836@qq.com>
2017-07-24 15:39:18 +0800

    王晋标  新的提交7、24

--- WXMovie/AppDelegate.m
+++ WXMovie/AppDelegate.m
@@ -3,7 +3,7 @@
 //  WXMovie
 //
 //  Created by imac on 15/9/7.
-//  Copyright (c) 2334 年 www.iphonetrain.com 无限互联3G学院. All rights reserved.
+//  Copyright (c) zhnede keyima  年 www.iphonetrain.com 无限互联3G学院. All rights reserved.
 //
 
 #import "AppDelegate.h"
